2892737edc27263177675291a6460846a36fcf88
Camilo Andres Garatejo Moreno <cgaratej@cbr6s1.42barcelona.com>
2024-02-29 16:02:45 +0100

    add fizzbuzz

---
+++ Level1/fizzbuzz/README.md
@@ -0,0 +1,42 @@
+## Sujeto
+
+```
+Nombre del programa: fizzbuzz
+Archivos esperados: fizzbuzz.c
+Funciones permitidas: write
+-------------------------------------------------- ------------------------------
+
+Escriba un programa que imprima los números del 1 al 100, cada uno separado por un
+nueva línea.
+
+Si el número es múltiplo de 3, en su lugar imprime "fizz".
+
+Si el número es múltiplo de 5, en su lugar imprime "buzz".
+
+Si el número es múltiplo de 3 y múltiplo de 5, en su lugar imprime "fizzbuzz".
+
+Ejemplo:
+
+$>./fizzbuzz
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+[...]
+97
+98
+fizz
+buzz
+$>
+```
---
+++ Level1/fizzbuzz/fizzbuzz.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fizzbuzz.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgaratej <cgaratej@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/29 15:45:44 by cgaratej          #+#    #+#             */
+/*   Updated: 2024/02/29 16:01:37 by cgaratej         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int num)
+{
+	if (num >=10)
+		ft_putnbr(num / 10);
+	ft_putchar(num % 10 + '0');
+}
+
+int	main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (i % 3 == 0)
+			write(1, "fizz", 4);
+		else if (i % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			ft_putnbr(i);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
